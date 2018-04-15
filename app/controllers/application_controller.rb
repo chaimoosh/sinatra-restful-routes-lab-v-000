fdf6f089c13a42baedaac939d09a74c6cfd128b9
Chaim Reisman <crreisman@gmail.com>
2018-04-15 19:35:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  delete '/recipes/:id/delete' do 
+  delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
-    @recipe.delete 
+    @recipe.delete
     redirect to '/recipes'
-  end 
+  end
 end
